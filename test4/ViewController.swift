7f6b0622dabc4508457c5e4be617e86b32ede439
ivinokurov <3f0ca5370c748585c1f9c72ed21d30e44f0561af>
2021-04-18 08:56:04 +0300

    Initial Commit

---
+++ test4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test4
+//
+//  Created by И.В. Винокуров on 18.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
